15166db5a1b0740e990e74e2cdccdc82c4c63b9f
Kilmar Eliezer García Pacheco <kilmar.999@gmail.com>
2021-07-09 19:27:54 -0600

    nginx server

--- f.sh
+++ f.sh
@@ -1,3 +1,15 @@
 cat ~/.ssh/poketo-pay.pub
 
 ssh root@147.182.133.205
+
+sudo mkdir -p /var/www/pay.poketo.link/html
+
+sudo chown -R $USER:$USER /var/www/pay.poketo.link/html
+
+sudo chmod -R 755 /var/www/pay.poketo.link
+
+nano /var/www/pay.poketo.link/html/index.html
+
+sudo nano /etc/nginx/sites-available/pay.poketo.link
+
+
